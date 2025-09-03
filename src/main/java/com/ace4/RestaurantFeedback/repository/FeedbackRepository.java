a1225aae1356dd10afa5595dc8c7d6b75dd76a1f
jeanPMartins <jean.martins008@gmail.com>
2025-09-02 23:18:57 -0300

    feat: new findBy added to future implementations

--- src/main/java/com/ace4/RestaurantFeedback/repository/FeedbackRepository.java
+++ src/main/java/com/ace4/RestaurantFeedback/repository/FeedbackRepository.java
@@ -3,5 +3,8 @@ package com.ace4.RestaurantFeedback.repository;
 import com.ace4.RestaurantFeedback.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    List<Feedback> findByAttendantName(String attendantName);
 }
